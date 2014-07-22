7f685a791c211f759a49fca32b718f0d9a914040
Dale Visser <dvisser@ida.org>
2014-07-21 20:22:21 -0400

    Added port specification and verbose instructions for dealing with MySQL.

--- start.sh
+++ start.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-java -jar jenkins-winstone.jar --warfile=wavsep.war --useJasper --commonLibFolder=lib
+WAVSEP_PORT=8844
+AJP13_PORT=8845
+echo "Welcome to pico-wavsep. Here are some instructions to properly
+configure your wavsep instance:
+
+1. Make sure you have MySQL 5.5 installed, noting the db admin username and
+   password.
+2. A database will be created in \"$HOME/db\".
+3. Browse to http://localhost:$WAVSEP_PORT/wavsep/wavsep-install/install.jsp
+4. Enter the previously noted db admin credentials in the form.
+"
+java -jar jenkins-winstone.jar --warfile=wavsep.war --useJasper \
+     --commonLibFolder=lib --httpPort=$WAVSEP_PORT --ajp13Port=$AJP13_PORT
