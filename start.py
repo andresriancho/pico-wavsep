f9e8c528c2a1f2f350533b4452e8ed225b37aebf
Dale Visser <dvisser@ida.org>
2014-07-22 18:45:58 -0400

    Corrected URL and db folder path in printed help. Since db folder lands in repo folder, added entry to .gitignore.

--- start.py
+++ start.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python
 
-from os import environ
-
-HOME = environ['HOME']
 WAVSEP_PORT = 8844
 AJP13_PORT = 8845
+
+from os import getcwd
+
 message = '''Welcome to pico-wavsep. Here are some instructions to properly
 configure your wavsep instance:
 
 1. Make sure you have MySQL 5.5 installed, noting the db admin username and
    password.
 2. A database will be created in "{}/db".
-3. Browse to http://localhost:{}/wavsep/wavsep-install/install.jsp
+3. Browse to http://localhost:{}/wavsep-install/install.jsp
 4. Enter the previously noted db admin credentials in the form.
-'''.format(HOME, WAVSEP_PORT)
+'''.format(getcwd(), WAVSEP_PORT)
 
 print message
 
