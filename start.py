bbfe568d2692dcd01188e21621a9d501baca8b64
Dale Visser <dvisser@ida.org>
2014-07-23 11:46:29 -0400

    Much more reasonable way to handle the subprocess, keeping an object reference to it, and freeing us up to make a request.

--- start.py
+++ start.py
@@ -5,25 +5,32 @@ AJP13_PORT = 8845
 
 from os import getcwd
 
+installURL = \
+    'http://localhost:{}/wavsep-install/install.jsp'.format(WAVSEP_PORT)
 message = '''Welcome to pico-wavsep. Here are some instructions to properly
 configure your wavsep instance:
 
 1. Make sure you have MySQL 5.5 installed, noting the db admin username and
    password.
 2. A database will be created in "{}/db".
-3. Browse to http://localhost:{}/wavsep-install/install.jsp
+3. Browse to "{}".
 4. Enter the previously noted db admin credentials in the form.
-'''.format(getcwd(), WAVSEP_PORT)
+'''.format(getcwd(), installURL)
 
 print message
 
-from subprocess import call,CalledProcessError
-try:
-    call(['java', '-jar', 'jenkins-winstone.jar', '--warfile=wavsep.war',
-	      '--useJasper', '-commonLibFolder=lib', 
-	      '--httpPort={}'.format(WAVSEP_PORT),
-	      '--ajp13Port={}'.format(AJP13_PORT)])
-except KeyboardInterrupt as ki:
-	print "Process was interrupted (likely by CTRL+C)."
-except CalledProcessError as cpe:
-	print "Exited Java process with exit code: {}".format(cpe.returncode)
+from subprocess import Popen
+
+server = Popen(['java', '-jar', 'jenkins-winstone.jar', '--warfile=wavsep.war',
+   '--useJasper', '-commonLibFolder=lib', 
+   '--httpPort={}'.format(WAVSEP_PORT),
+   '--ajp13Port={}'.format(AJP13_PORT)])
+
+print 'Wavsep server process started with PID {}.'.format(server.pid)
+print
+print 'Press any key to terminate.'
+
+from sys import stdin
+
+stdin.read(1)
+server.terminate()
